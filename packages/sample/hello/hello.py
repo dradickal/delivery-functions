d63178f74fb471a344433527f6643661ac320edb
David Radick <dradick@radickalcreations.com>
2026-04-19 11:42:39 -0500

    return function runtime from sample function

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,5 +1,21 @@
-def main(args):
-      name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!"
-      print(greeting)
-      return {"body": greeting}
+def main(event, context):
+      print(f'{{ requestId: {context.request_id}, function: {context.namespace}/{context.function_name}, version: {context.function_version} }}')
+      return {
+            "body": {
+                  "event": event,
+                  "context": {
+                        "activationId": context.activation_id,
+                        "apiHost": context.api_host,
+                        "apiKey": context.api_key,
+                        "deadline": context.deadline,
+                        "functionName": context.function_name,
+                        "functionVersion": context.function_version,
+                        "namespace": context.namespace,
+                        "requestId": context.request_id,
+                  },
+            },
+            "statusCode": 200,
+		"headers": {
+			"Content-Type": "application/json"
+		}
+      }
